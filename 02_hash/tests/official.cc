29aab9fb76d536d87e8aa9bf38e97e1d55d0010e
Kwasow <wasowski02@protonmail.com>
2022-10-26 23:23:17 +0200

    Made test logs clean (#18)

--- 02_hash/tests/official.cc
+++ 02_hash/tests/official.cc
@@ -1,20 +1,24 @@
 #include "gtest/gtest.h"
 #include "hash.h"
 
+#include <iostream>
+
 namespace {
 
-    using ::std::data;
+using ::std::data;
 
-    uint64_t hash_function(uint64_t const * v, size_t n) {
-        uint64_t hash = 0;
-        for (size_t k = 0; k < n; ++k)
-          hash ^= v[k];
-        return hash;
-    }
+uint64_t hash_function(uint64_t const * v, size_t n) {
+    uint64_t hash = 0;
+    for (size_t k = 0; k < n; ++k)
+      hash ^= v[k];
+    return hash;
+}
 
 } /* anonymous namespace */
 
 TEST(Official, General) {
+    testing::internal::CaptureStderr();
+
     auto id = ::jnp1::hash_create(hash_function);
     EXPECT_EQ(::jnp1::hash_insert(id, data<uint64_t>({1, 3, 5, 9}), 4), true);
 
@@ -24,4 +28,6 @@ TEST(Official, General) {
     EXPECT_EQ(::jnp1::hash_size(id), 2);
     ::jnp1::hash_delete(id);
     EXPECT_EQ(::jnp1::hash_test(id, nullptr, 0), false);
+    
+    std::string output = testing::internal::GetCapturedStderr();
 }
