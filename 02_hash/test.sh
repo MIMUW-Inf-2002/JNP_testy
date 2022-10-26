582db422c2f24c8040817e8fe0ed6ba0454b9ea3
Mateusz Wasilewski Ubuntu <mateuszwasilewski12@gmail.com>
2022-10-26 20:06:20 +0200

    Test script no longer purges CMake cache

--- 02_hash/test.sh
+++ 02_hash/test.sh
@@ -5,11 +5,12 @@ FILEDIR=${1}
 [ -z ${FILEDIR} ] && printf "Nie podałeś folderu z plikami!\n" && exit 1
 
 rm -f hash.cc hash.h
-ln -s $FILEDIR/hash.cc hash.cc
-ln -s $FILEDIR/hash.h hash.h
+ln $FILEDIR/hash.cc hash.cc || exit 1
+ln $FILEDIR/hash.h hash.h || exit 1
 
-rm -rf build
-mkdir build
+[ ! -d "build" ] && mkdir build
+
 cd build
 cmake ..
+
 cmake --build .
