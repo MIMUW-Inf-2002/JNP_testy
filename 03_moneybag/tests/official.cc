861a66a3d8c89393e021839758e95a9438577ec6
Rentib <70008415+Rentib@users.noreply.github.com>
2022-11-09 08:43:17 +0100

    testy oficjalne i własne do zadania 03 moneybag (#22)

---
+++ 03_moneybag/tests/official.cc
@@ -0,0 +1,66 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+
+#include "moneybag.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(Peczar, official) {
+  // Taka konstrukcja nie powinna się kompilować.
+  // Moneybag m0;
+
+  Moneybag m1 = Moneybag(1, 10, 19);
+  Moneybag m2 = 2 * Solidus + 17 * Denier;
+  Moneybag m3(0, 0, 0);
+  Moneybag m4(m1);
+  m4 = m3;
+
+  cout << m3 << endl;
+  cout << m4 + m2 - Solidus << endl;
+
+  Moneybag::coin_number_t s = m2.solidus_number();
+  ASSERT_TRUE(s == 2);
+
+  ASSERT_TRUE(m1);
+  ASSERT_TRUE(!m3);
+  ASSERT_TRUE(m1 >= m2);
+  ASSERT_TRUE(m1 > m2);
+  ASSERT_TRUE(!(m1 < m2));
+  ASSERT_TRUE(m4 == m3);
+
+  ASSERT_TRUE(Moneybag(2, 2, 1) >= Moneybag(2, 1, 1));
+  ASSERT_TRUE(Moneybag(2, 2, 1) > Moneybag(2, 1, 1));
+  ASSERT_FALSE(Moneybag(1, 2, 2) <= Moneybag(2, 2, 1));
+  ASSERT_FALSE(Moneybag(1, 2, 2) < Moneybag(2, 2, 1));
+
+  m2 += 28 * Solidus + 2 * Denier;
+
+  cout << m1 << endl;
+  cout << m2 << endl;
+
+  ASSERT_FALSE(m1 == m2);
+  ASSERT_FALSE(m1 > m2);
+  ASSERT_FALSE(m1 < m2);
+
+  Value v1 = Value(m1);
+  Value v2(m2);
+
+  cout << static_cast<string>(v1) << endl;
+  cout << string(Value(Livre)) << endl;
+
+  ASSERT_TRUE(v1 == v2);
+  ASSERT_TRUE(!is_neq(v1 <=> v2));
+  ASSERT_TRUE(v1 == 379);
+  ASSERT_TRUE(379 == v2);
+  ASSERT_TRUE(v1 > 350);
+  ASSERT_TRUE(350 < v2);
+  ASSERT_TRUE(0 == Value());
+
+  // Te konstrukcje nie powinny się kompilować.
+  // Livre = Denier;
+  // Denier = Solidus;
+  // Solidus = Livre;
+  // int k = Moneybag(1, 2, 3);
+}
---
+++ 03_moneybag/tests/rentib.cc
@@ -0,0 +1,143 @@
+#include <sstream>
+
+#include "moneybag.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(Rentib, binary_operators) {
+  Moneybag a(1, 1, 1);
+  Moneybag b(2, 2, 2);
+  a = a * 3 + a - b;
+  b = b * 2 + a;
+  ASSERT_TRUE(a < b);
+  ASSERT_TRUE(a <= b);
+  ASSERT_FALSE(a >= b);
+
+  a = Moneybag(1, 1, 1);
+  b = Moneybag(2, 2, 2);
+
+  {
+    bool exceptionThrown = false;
+    try {
+      a = a - b;
+    } catch (...) {
+      exceptionThrown = true;
+    }
+    ASSERT_TRUE(exceptionThrown);
+    ASSERT_TRUE(a == Moneybag(1, 1, 1));
+  }
+
+  {
+    bool exceptionThrown = false;
+    try {
+      a -= b;
+    } catch (...) {
+      exceptionThrown = true;
+    }
+    ASSERT_TRUE(exceptionThrown);
+    ASSERT_EQ(a, Moneybag(1, 1, 1));
+  }
+
+  b -= a;
+  ASSERT_TRUE(a <= b);
+  ASSERT_TRUE(a >= b);
+  ASSERT_EQ(a, b);
+  ASSERT_EQ(a * 213, 213 * a);
+  ASSERT_EQ(a * 213, 213 * b);
+
+  volatile int x = 2;
+  ASSERT_EQ(a * (bool)x, a);
+  ASSERT_EQ(a * (unsigned char)x, a + a);
+  ASSERT_EQ(a * (unsigned short)x, a + a);
+  ASSERT_EQ(a * (unsigned int)x, a + b - b + a);
+  ASSERT_EQ(a * (unsigned long)x, a + b);
+  ASSERT_EQ(a * (unsigned long long)x, a + b - a + b);
+  ASSERT_EQ(a * (uint8_t)x, a + a);
+  ASSERT_EQ(a * (uint16_t)x, a + b - b + a);
+  ASSERT_EQ(a * (uint32_t)x, a + b);
+  ASSERT_EQ(a * (uint64_t)x, a + b - a + b);
+}
+
+TEST(Rentib, comparision) {
+  Moneybag a(1, 2, 3);
+  Moneybag b(3, 2, 1);
+
+  ASSERT_FALSE(a < b);
+  ASSERT_FALSE(a > b);
+  ASSERT_FALSE(a >= b);
+  ASSERT_FALSE(a <= b);
+  ASSERT_FALSE(a == b);
+
+  a = Moneybag(1, 1, 1);
+  b = a;
+  ASSERT_TRUE(a <= b);
+  ASSERT_TRUE(a >= b);
+  ASSERT_TRUE(a == b);
+
+  a = Moneybag(1, 1, 3);
+  b = a * 2;
+  ASSERT_TRUE(a < b);
+  ASSERT_TRUE(a <= b);
+  ASSERT_TRUE(a != b);
+
+  Value v = Value(a);
+  Value u(b);
+
+  ASSERT_TRUE(v < u);
+  ASSERT_TRUE(v <= u);
+  ASSERT_FALSE(v == u);
+  ASSERT_FALSE(v >= u);
+  ASSERT_FALSE(v > u);
+
+  ASSERT_TRUE(v < (1 << 8));
+  ASSERT_TRUE(v > (1 << 8) - 2);
+  ASSERT_TRUE(v == ~(~0 << 8));
+}
+
+TEST(Rentib, constructor) {
+  Moneybag m1(1, 2, 3);
+  Moneybag m2 = Moneybag(3, 2, 1);
+  Moneybag m3(m1);
+  Moneybag m4 = Solidus;
+  m1 = m2;
+  m3 = m4;
+  Value v1(10);
+  Value v2(m1);
+  Value v3 = v1;
+  v2 = v3;
+}
+
+TEST(Rentib, getters) {
+  Moneybag a(1, 1, 1);
+  ASSERT_EQ(a.livre_number(), 1);
+  ASSERT_EQ(a.solidus_number(), 1);
+  ASSERT_EQ(a.denier_number(), 1);
+  ASSERT_EQ(a.livre_number(), Moneybag(1, 10, 100).livre_number());
+  ASSERT_EQ(a.solidus_number(), Moneybag(2, 5, 1).denier_number());
+}
+
+TEST(Rentib, strings) {
+  Moneybag m(0, 0, 0);
+
+  const string mout[4] = {
+      "(1 livr, 2 soliduses, 2 deniers)",
+      "(2 livres, 1 solidus, 2 deniers)",
+      "(2 livres, 2 soliduses, 1 denier)",
+      "(2 livres, 2 soliduses, 2 deniers)",
+  };
+
+  for (int i = 0; i < 4; i++) {
+    stringstream buf;
+    m = Moneybag(2 - (i == 0), 2 - (i == 1), 2 - (i == 2));
+    buf << m;
+    ASSERT_EQ(buf.str(), mout[i]);
+  }
+
+  ASSERT_EQ(string(Value(0)), "0");
+  ASSERT_EQ(string(Value(~0)), "18446744073709551615");
+  ASSERT_EQ(string(Value((uint8_t)~0)), "255");
+  ASSERT_EQ(string(Value((uint16_t)~0)), "65535");
+  ASSERT_EQ(string(Value((uint32_t)~0)), "4294967295");
+  ASSERT_EQ(string(Value((uint64_t)~0)), "18446744073709551615");
+}
